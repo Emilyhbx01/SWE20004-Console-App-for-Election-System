3fe4ee7936cf37a86f3c32a8781c01311ee3835f
unknown <emilyhbx01@gmail.com>
2021-12-09 11:29:04 +0800

    final release

--- SWE20004_Project/SWE20004_Project/main.cpp
+++ SWE20004_Project/SWE20004_Project/main.cpp
@@ -15,58 +15,58 @@ Program Description: This program is a voting system that consists of 2 database
 using namespace std;
 
 
-int main() {
-	bool run = true;
-
-	while (run) {
-		displayVoteMenu();
-		string voteChoice = promptVoteChoice();
-		Voter voter;
-
-		if (voteChoice == "1") {
-			voter.viewCandidatesOptions();	//Menu option 1 that is to view candidates
-		}								   	
-		else if (voteChoice == "2") {	   
-			voter.registerVoter();			//Menu option 2 that is to register voters
-		}								   								  
-		else if (voteChoice == "3") {	   
-			voter.vote();					//Menu option 3 that is to vote
-		}								   
-		else if (voteChoice == "4") {	   
-			voter.viewVoteResult();			//Menu option 4 is to view the result of the voting
-		}								   
-		else if (voteChoice == "5") {	   
-			voter.quit();					//Menu option 5 is to quit the program
-			run = false;
-			return 1;
-		}
-	}
-
-	return 0;
-}
-
-
-////Main function of the program
 //int main() {
 //	bool run = true;
 //
 //	while (run) {
-//		displayAddCandidateMenu();
-//		string add_choice = promptChoice();
-//		if (add_choice == "1") {
-//			addCandidate(); //adding candidate function
-//		}
-//		else if (add_choice == "2") {
-//			viewCandidate(); //viewing candidate function
-//		}
-//		else if (add_choice == "3") {
-//			searchCandidate(); //search candidate function
-//		}
-//		else if (add_choice == "4") {
-//			exitProgram();//quit the program
+//		displayVoteMenu();
+//		string voteChoice = promptVoteChoice();
+//		Voter voter;
+//
+//		if (voteChoice == "1") {
+//			voter.viewCandidatesOptions();	//Menu option 1 that is to view candidates
+//		}								   	
+//		else if (voteChoice == "2") {	   
+//			voter.registerVoter();			//Menu option 2 that is to register voters
+//		}								   								  
+//		else if (voteChoice == "3") {	   
+//			voter.vote();					//Menu option 3 that is to vote
+//		}								   
+//		else if (voteChoice == "4") {	   
+//			voter.viewVoteResult();			//Menu option 4 is to view the result of the voting
+//		}								   
+//		else if (voteChoice == "5") {	   
+//			voter.quit();					//Menu option 5 is to quit the program
 //			run = false;
 //			return 1;
 //		}
 //	}
+//
 //	return 0;
 //}
+
+
+//Main function of the program
+int main() {
+	bool run = true;
+
+	while (run) {
+		displayAddCandidateMenu();
+		string add_choice = promptChoice();
+		if (add_choice == "1") {
+			addCandidate(); //adding candidate function
+		}
+		else if (add_choice == "2") {
+			viewCandidate(); //viewing candidate function
+		}
+		else if (add_choice == "3") {
+			searchCandidate(); //search candidate function
+		}
+		else if (add_choice == "4") {
+			exitProgram();//quit the program
+			run = false;
+			return 1;
+		}
+	}
+	return 0;
+}
